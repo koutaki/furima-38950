184fd550b617625baa10784339fac6be367ed117
koutaki <yu.ya.1.1eve@gmail.com>
2023-06-19 01:31:11 +0900

    商品情報編集機能
    １回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.nil?
+      redirect_to new_user_session_path, alert: 'ログインしてください'
+    elsif current_user == @item.user 
+      render :edit
+    else
+      redirect_to root_path, alert: '商品の編集はできません。'
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if current_user == @item.user 
+      if @item.update(item_params)
+        redirect_to @item, notice: '商品が正常に更新されました。'
+      else       
+        render :edit
+      end
+    else      
+      redirect_to root_path, alert: '商品の編集はできません。'
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :price, :item_condition_id, :introduction, :category_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :image)
